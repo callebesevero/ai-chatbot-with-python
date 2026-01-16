95f1fb204db8ee09705537e4cb7a6b87b21e4fdd
Callebe Severo <callebesev@gmail.com>
2026-01-16 12:37:02 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,32 +1,60 @@
-# session_state -> cookies do navegador -> (armazena) lista de mensagens
-# chat_input -> usuário escreve
-# chat_message -> mensagem no chat
-# write -> mostra no front
 from streamlit import chat_input, chat_message, write, session_state, set_page_config, title, subheader
 from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
+from os import getenv
 
-set_page_config(page_title='ChatBot Callebe', page_icon='😄')
-title('Chatbot Callebe')
+set_page_config(page_title='ChatBot Callebe', page_icon='😁')
+title('Chatbot Callebe 😁')
+
+load_dotenv()
+hf_token = getenv('HF_TOKEN')
+client = InferenceClient(
+    model='meta-llama/Llama-3.2-3B-Instruct',
+    token=hf_token
+)
 
 if not 'message_list' in session_state:
     session_state['message_list'] = []
 
+for mss in session_state['message_list']:
+    chat_message(avatar=mss['avatar'], name=mss['name'], width='content').write(mss['content'])
+
 message = chat_input('Type your ask:')
 if message:
-    message_dict = {
+    chat_message(name='user', avatar='user', width='content').write(message)
+    user_message_dict = {
         'avatar': 'user',
         'name': 'user',
-        'message': message
+        'content': message
     }
-    session_state['message_list'].append(message_dict)
 
-    ai_message = ...
+    session_state['message_list'].append(user_message_dict)
+    
+    api_messages = [
+        {
+            'role': 'user' if m['avatar'] == 'user' else 'assistant', 'content': m['content']
+        }
+        for m in session_state['message_list']
+    ]
+
+    stream = client.chat_completion(
+        messages=session_state['message_list'],
+        max_tokens=500,
+        stream=True
+    )
+
+    ai_response = ''
+
+    for chunk in stream:
+        token = chunk.choices[0].delta.content
+        if token:
+            ai_response += token
+
     ai_dict = {
         'avatar': 'ai',
         'name': 'ai',
-        'message': ai_message
+        'content': ai_response
     }
-    session_state['message_list'].append(ai_dict)
 
-    for mss in session_state['message_list']:
-        chat_message(avatar=mss['avatar'], name=mss['name'], width='content').write(mss['message'])
+    chat_message(name='ai', avatar='ai', width='content').write(ai_response)
+    session_state['message_list'].append(ai_dict)
